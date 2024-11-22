210741a1ea9cc02b4e13b300397c1c4ac462937a
Yassir Ait Ammi <yaait-am@e1r11p8.1337.ma>
2024-11-22 14:23:41 +0100

    dvds

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,31 @@
 /*   By: yaait-am <yaait-am@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:37:47 by yaait-am          #+#    #+#             */
-/*   Updated: 2024/11/12 14:37:56 by yaait-am         ###   ########.fr       */
+/*   Updated: 2024/11/22 11:12:42 by yaait-am         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
+
 char	*get_next_line(int fd)
 {
-	return (NULL);
+	int	i;
+	char	readed[BUFFER_SIZE + 1];
+	static char	*save;
+
+	i = 0;
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	while ((i = read(fd, readed, BUFFER_SIZE)) > 0)
+	{
+		readed[i] = '\0';
+		save = ft_strjoin(save, readed);
+		if (ft_strchar(readed, '\n'))
+			break ;
+ 	}
+	if (i < 0 || (!save && i == 0))
+		return (NULL);
+	return(ft_line(&save));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,20 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaait-am <yaait-am@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: yaait-am <yaait-am@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:37:08 by yaait-am          #+#    #+#             */
-/*   Updated: 2024/11/12 14:37:12 by yaait-am         ###   ########.fr       */
+/*   Updated: 2024/11/21 14:35:58 by yaait-am         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 7
+#endif
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *src);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 
 #endif
